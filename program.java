bd3d943ba841020f3262563fb16f2572b8bfc04f
Aaku765 <90411974+Aaku765@users.noreply.github.com>
2022-08-08 22:38:43 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class GFG extends Thread {
+	public void run()
+	{
+		System.out.print("Welcome to GeeksforGeeks.");
+	}
+	public static void main(String[] args)
+	{
+		GFG g = new GFG(); // creating thread
+		g.start(); // starting thread
+	}
+}
